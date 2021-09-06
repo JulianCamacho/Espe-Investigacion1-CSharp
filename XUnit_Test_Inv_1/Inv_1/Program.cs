bae256cf6a36fff5b9feb5e4c61f0eb18d248dd0
Julian <jcamacho341@estudiantec.cr>
2021-09-06 14:07:41 -0600

    Se agregan comentarios, una función y su rueba unitaria

--- XUnit_Test_Inv_1/Inv_1/Program.cs
+++ XUnit_Test_Inv_1/Inv_1/Program.cs
@@ -15,8 +15,6 @@ namespace Inv_1
     {
         public string name { get; set; }
         public string email { get; set; }
-        public string password { get; set; }
-        public int id { get; set; }
         public int salary { get; set; }
 
         /// <summary>
@@ -52,20 +50,31 @@ namespace Inv_1
             return newSalary;
         }
 
+
+        /// <summary>
+        /// Función para verificar si un email es de una compañía
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public bool checkCompanyEmail(string email)
+        {
+            return email.EndsWith("@estudiantec.cr");
+        }
+
     }
 
-    class UserCollection {
+    public class EmployeeDataBase {
 
 
         /// <summary>
-        /// Función para obtener el usuario en la posición proporcionada
+        /// Función para obtener el empleado en la posición proporcionada
         /// </summary>
-        /// <param name="users"> Lista de todos los usuarios </param>
+        /// <param name="employees"> Lista de todos los empleados </param>
         /// <param name="index"> Posición en la lista por retornar </param>
-        /// <returns> El usuario en la posición requerida </returns>
-        public static Employee getUserAt(Employee[] users, int index) {
+        /// <returns> El empleado en la posición requerida </returns>
+        public static Employee getEmployeeAt(Employee[] employees, int index) {
 
-            return users[index];
+            return employees[index];
         }
     }
 }
--- XUnit_Test_Inv_1/XUnit_Test_Inv_1/UnitTest1.cs
+++ XUnit_Test_Inv_1/XUnit_Test_Inv_1/UnitTest1.cs
@@ -1,11 +1,18 @@
 using Inv_1;
 using System;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace XUnit_Test_Inv_1
 {
     public class UnitTest1
     {
+
+        /// <summary>
+        /// Función de prueba verificar si un email es validado correctamente
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="email"></param>
         [Theory]
         [InlineData(true, "jjulian.341@gmail.com")]
         [InlineData(false, "braalfa.com")]
@@ -19,6 +26,13 @@ namespace XUnit_Test_Inv_1
             Assert.Equal(expected, isValid);
         }
 
+
+        /// <summary>
+        /// Función de prueba para verificar si se calcula el nuevo salario correctamente
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="currentSalary"></param>
+        /// <param name="incrementPorcentage"></param>
         [Theory]
         [InlineData(5250, 5000, 5)]
         [InlineData(108, 100, 8)]
@@ -31,6 +45,11 @@ namespace XUnit_Test_Inv_1
             Assert.Equal(expected, newSalary);
         }
 
+
+        /// <summary>
+        /// Función de prueba para verificar si se genera una excepción 
+        /// si no se proporciona un email
+        /// </summary>
         [Fact]
         public void throwExceptionEmailNull()
         {
@@ -39,6 +58,51 @@ namespace XUnit_Test_Inv_1
             Assert.Throws<Exception>(() => emp.isValidEmail(null));
         }
 
+        /// <summary>
+        /// Función para verificar que se obtiene el empleado correcto de una lista
+        /// </summary>
+        [Fact]
+        public void validateGetUserFromList()
+        {
+            //Arrange
+            Employee[] employees = new Employee[3] {
+                new Employee { name = "José", email = "aaaa@aaaa.com", salary = 700 },
+                new Employee { name = "María", email = "bbbb@bbbb.com", salary = 1500 },
+                new Employee { name = "Isabel", email = "cccc@cccc.com", salary = 650 }
+            };
 
+            int index = 2;
+            Employee expected = employees[2];
+
+            //Act
+            Employee actual = EmployeeDataBase.getEmployeeAt(employees, index);
+
+            //Assert
+            Assert.NotEmpty(employees);
+            Assert.IsType<Employee>(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Función de prueba para verificar que un email
+        /// corresponde a un correo institucional
+        /// </summary>
+        /// <param name="email"></param>
+        [Theory]
+        //[InlineData("jjulian.341@gmail.com")] //No pasa la prueba
+        [InlineData("jcamacho@estudiantec.cr")]
+        [InlineData("braalfa@estudiantec.cr")]
+        public void validateCompanyEmails(string email)
+        {
+            //Arrange
+            var emp = new Employee();
+
+            //Act
+            bool isCompanyEmail = emp.checkCompanyEmail(email);
+
+            //Assert
+            Assert.EndsWith("estudiantec.cr", email);
+            Assert.True(isCompanyEmail);
+        }
     }
 }
